469a7a90fd591230b6166142646a1922bca85202
BastosDiogo <bmnetto.diogo@gmail.com>
2023-01-31 17:38:58 -0300

    Add Módulo mongo e módulo usuario.

---
+++ model/conexao_mongo.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+
+import pymongo
+
+load_dotenv()
+
+class Pymongo:
+    def __init__(self) -> None:
+        MONGO_HOST = os.getenv("MONGO_HOST")
+        MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+        MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+        self.client = pymongo.MongoClient(
+            f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}{MONGO_HOST}",
+            tls=True,
+            tlsCAFile="isrgrootx1.pem",
+            tlsAllowInvalidHostnames=True,
+            retryWrites=False,)
+        self.database = self.client[os.getenv("DATABASE_ENVIROMENT")]
+
+# x = list(
+#     Pymongo().database['usuario'].find(
+#         {},
+#         {"_id":0}
+#     )
+# )
+
+# print(x)
